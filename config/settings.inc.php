23b225d50c60583b54a24a61f9f670ccb96068d4
dimagvk <dimagvk@gmail.com>
2013-04-06 16:20:06 +0300

    Config settings

---
+++ config/settings.inc.php
@@ -0,0 +1,18 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'password');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', '8bGHk40x0Cyish8W7bnytuYvV514zZ59jdbCOgGHJObb5ZAWgswVQi9Z');
+define('_COOKIE_IV_', 'EyjczoXO');
+define('_PS_CREATION_DATE_', '2013-02-17');
+define('_PS_VERSION_', '1.5.3.1');
+define('_RIJNDAEL_KEY_', 'BBnBJuRF4ELjJEWbzru2gqPR43JE3zdN');
+define('_RIJNDAEL_IV_', '5D2gelgowfRTNBIYGi37iw==');
